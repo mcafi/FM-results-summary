dfbcc9c8b1ef844cef09eb84aded261253e540a8
Matteo Carniglia <m.carniglia@wikicasa.it>
2023-03-24 12:58:39 +0100

    progress

---
+++ src/components/TotalScore.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TotalScore() {
+  return (
+    <div className="TotalScore">
+        <div className="total-score">76</div>
+        <div className="max-score">of 100</div>
+    </div>
+  )
+}
