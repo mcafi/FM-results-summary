67c4282651ad80f883824f9ccd98c10540d47717
Matteo Carniglia <m.carniglia@wikicasa.it>
2023-04-04 09:54:18 +0200

    test gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import Result from './components/Result'
+import Summary from './components/Summary'
 
 function App() {
 
   return (
     <div className="App">
       <Result />
-      <div className="scores-container">
-        Details
-      </div>
+      <Summary />
     </div>
   )
 }
---
+++ src/components/Summary.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Summary() {
+  return (
+    <div className="Summary">
+        <h3>Summary</h3>
+        <div className="score score-1">
+            1
+        </div>
+        <div className="score score-2">
+            2
+        </div>
+        <div className="score score-3">
+            3
+        </div>
+        <div className="score score-4">
+            4
+        </div>
+        <button className="button">Continue</button>
+    </div>
+  )
+}
